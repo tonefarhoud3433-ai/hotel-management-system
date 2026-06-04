d731d430711856f17799fcff53b3c7993dc6ea7c
Ahmed-Adel-dev21 <ahmedadelali0155@gemail.com>
2026-06-05 02:58:54 +0300

    ui register and verify

--- src/Layouts/AuthLayout/AuthLayout.tsx
+++ src/Layouts/AuthLayout/AuthLayout.tsx
@@ -1,4 +1,4 @@
-import Grid from "@mui/material/Grid";
+import Grid from "@mui/material/Grid"; // أو المسار المعتمد عندك
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { Outlet } from "react-router-dom";
@@ -6,77 +6,129 @@ import bgImg1 from "../../assets/Images/Auth/authbg1.png";
 
 export default function AuthLayout() {
   return (
-    <Grid
-      container
+    <Box 
       sx={{
-        height: "100vh",
-        p: 2,
+        height: "100vh", 
+        display: "flex",
+        flexDirection: "column",
+        p: { xs: 2, md: 3 },
         boxSizing: "border-box",
+        backgroundColor: "#ffffff",
+        overflow: "hidden" ,
       }}
     >
-      {/* Form Section */}
-      <Grid
-        size={{ xs: 12, md: 6 }}
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Box
-          sx={{
-            width: "100%",
-            maxWidth: 500,
+      {/* logo */}
+      <Box sx={{ mb: 1 }}> 
+        <Typography 
+          variant="h5" 
+          sx={{ 
+            fontWeight: 700, 
+            color: "#3b5bdb", 
+            fontFamily: "sans-serif"
           }}
         >
-          <Outlet />
-        </Box>
-      </Grid>
+          Stay<Box component="span" sx={{ color: "rgba(21, 44, 91, 1)" }}>cation.</Box>
+        </Typography>
+      </Box>
 
-      {/* Image Section */}
+      {/*  Parent Grid  */}
       <Grid
-        size={{ xs: 0, md: 6 }}
+        container
+        spacing={{ xs: 2, md: 4 }}
         sx={{
-          display: { xs: "none", md: "block" },
-          height: "100%",
+          flexGrow: 1, 
+          height: "calc(100% - 25px)",
+          alignItems: "center",
+          boxSizing: "border-box",
         }}
       >
-        <Box
+        {/* Form Section  */}
+        <Grid
+          size={{ xs: 12, md: 6 }}
           sx={{
-            position: "relative",
-            width: "100%",
-            height: "100%",
-            borderRadius: 4,
-            overflow: "hidden",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            height: "100%", 
+            maxHeight: "100%",
+            p: { xs: 1, md: 3 },
           }}
         >
           <Box
-            component="img"
-            src={bgImg1}
-            alt="Background"
             sx={{
               width: "100%",
-              height: "100%",
-              objectFit: "cover",
-            }}
-          />
-
-          <Box
-            sx={{
-              position: "absolute",
-              bottom: 40,
-              left: 40,
-              color: "#fff",
+              maxWidth: 500,
+              mx: 'auto',
+              p: { xs: 2, md: 5 },
             }}
           >
-            <Typography variant="h4" sx={{ fontWeight: 700 }}>
-              Login
-            </Typography>
-
-            <Typography variant="body1">Homes as unique as you.</Typography>
+            <Outlet />
           </Box>
-        </Box>
+        </Grid>
+
+        {/* Image Section (الجانب الأيمن) */}
+        <Grid
+          size={{ xs: 0, md: 6 }}
+          sx={{
+            display: { xs: "none", md: "block" },
+            height: "100%", 
+            boxSizing: "border-box"
+          }}
+        >
+          <Box
+            sx={{
+              position: "relative",
+              width: "100%",
+              height: "100%", 
+              borderRadius: 5,
+              overflow: "hidden",
+              boxShadow: "0 10px 30px rgba(0,0,0,0.02)"
+            }}
+          >
+            <Box
+              component="img"
+              src={bgImg1}
+              alt="Background"
+              sx={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+              }}
+            />
+
+            <Box
+              sx={{
+                position: "absolute",
+                bottom: 40,
+                left: 40,
+                color: "#fff",
+                zIndex: 2
+              }}
+            >
+              <Typography variant="h4" sx={{ fontWeight: 700, mb: 1 }}>
+                Sign up to Roamhome
+              </Typography>
+              <Typography variant="body1" sx={{ opacity: 0.9 }}>
+                Homes as unique as you.
+              </Typography>
+            </Box>
+
+            
+            <Box 
+              sx={{
+                position: "absolute",
+                bottom: 0,
+                left: 0,
+                right: 0,
+                height: "40%",
+                background: "linear-gradient(to top, rgba(0,0,0,0.4), transparent)",
+                zIndex: 1
+              }}
+            />
+          </Box>
+        </Grid>
+
       </Grid>
-    </Grid>
+    </Box>
   );
 }
--- src/Modules/AuthModules/Verify/Verify.tsx
+++ src/Modules/AuthModules/Verify/Verify.tsx
@@ -1,3 +1,107 @@
+import {
+  Box,
+  Button,
+  Grid,
+  Link,
+  TextField,
+  Typography
+} from '@mui/material';
 export default function Verify() {
-  return <div>Verify</div>;
+    
+    const textFieldStyle = {
+  
+      '& .css-16wblaj-MuiInputBase-input-MuiOutlinedInput-input':{
+        padding:'10px 14px'
+      },
+      '& .css-1dune0f-MuiInputBase-input-MuiOutlinedInput-input':{
+        padding:'10px 14px'
+      },
+      '& .MuiOutlinedInput-root': {
+        backgroundColor: 'rgba(245, 246, 248, 1)',
+        borderRadius: '6px',
+        '& fieldset': { 
+          borderColor: '#e2e8f0',
+          borderWidth:'0px',
+        },
+        '&:hover fieldset': {
+          borderColor: '#cbd5e1',
+        },
+        '&.Mui-focused fieldset': { 
+          borderColor: 'rgba(21, 44, 91, 1)', 
+          borderWidth: '2px' 
+        }
+      }
+    };
+  return <>
+  <Box 
+      component="form" 
+      // onSubmit={} 
+      sx={{ width:'100%',  mx: 'auto', fontFamily: 'sans-serif' }}
+    >
+      {/* Header Section */}
+      <Typography variant="h4" gutterBottom sx={{ color: 'rgba(21, 44, 91, 1)', fontWeight: 'bold' }}>
+        Verify Your Account
+      </Typography>
+      
+      <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
+        If you already have an account register
+      </Typography>
+      <Typography variant="body1" sx={{ mb: 4 }}>
+        You can <Link href="/login" underline="none" sx={{ color: 'rgba(235, 81, 72, 1)', fontWeight: 'bold' }}>Login here !</Link>
+      </Typography>
+
+      {/* parent Grid  */}
+      <Grid container spacing={2}>
+        {/* Email Address */}
+        <Grid size={12}>
+          <Typography variant="subtitle2"  sx={{color:"rgba(21, 44, 91, 1)", mb: 1, fontWeight: 500 }}>
+            Email Address
+          </Typography>
+          <TextField
+            fullWidth
+            type="email"
+            placeholder="Please type here ..."
+            sx={textFieldStyle}
+          />
+        </Grid>
+       {/* OTP Code */}
+        <Grid size={12}>
+          <Typography variant="subtitle2"  sx={{color:"rgba(21, 44, 91, 1)", mb: 1, fontWeight: 500 }}>
+            OTP Code
+          </Typography>
+          <TextField
+            fullWidth
+            placeholder="Please type your OTP here ..."
+            sx={textFieldStyle}
+          />
+        </Grid>
+        <Grid size={12} sx={{ mt: 1 }}>
+          <Button
+            disableElevation
+            type="submit"
+            fullWidth
+            variant="contained"
+            size="large"
+            sx={{ 
+              backgroundColor: '#3b5bdb', 
+              py: 1, 
+              textTransform: 'none', 
+              fontSize: '17px',
+              fontWeight: '500',
+              borderRadius: '8px',
+              boxShadow: '0 4px 12px rgba(59, 91, 219, 0.2)',
+              '&:hover': { 
+                backgroundColor: '#2d46b3',
+                boxShadow: '0 6px 16px rgba(59, 91, 219, 0.3)'
+              }
+            }}
+          >
+            Send 
+          </Button>
+        </Grid>
+
+      </Grid>
+    </Box>
+  
+  </>;
 }
