775a8546571018e191c0183459fd349f4447d652
Abdelrahman Mohamed <abdelrhmanaboyehia@gmail.com>
2026-06-05 15:45:40 +0300

    forget & Reset Components

--- src/Modules/AuthModules/YupValidation/YupValidation.ts
+++ src/Modules/AuthModules/YupValidation/YupValidation.ts
@@ -37,6 +37,35 @@ export const verifySchema = yup.object({
     .email('Invalid email format'),
 
 }).required();
+export const forgetSchema = yup.object({
+  email: yup
+    .string()
+    .required('Email address is required')
+    .email('Invalid email format'),
+
+}).required();
+export const resetSchema = yup.object({
+  email: yup
+    .string()
+    .required('Email address is required')
+    .email('Invalid email format'),
+  code: yup
+    .string()
+    .required('OTP is required')
+    .length(4, 'OTP must be 4 characters'),
+
+  password: yup
+    .string()
+    .required('Password is required')
+    .min(6, 'Password must be at least 6 characters'),
+  confirmPassword: yup
+    .string()
+    .required('Please confirm your password')
+    .oneOf([yup.ref('password')], 'Passwords must match'),
+}).required();
+
 
 export type RegisterFormData = yup.InferType<typeof registerSchema>;
 export type VerifySchema = yup.InferType<typeof verifySchema>;
+export type ForgetSchema = yup.InferType<typeof forgetSchema>;
+export type ResetSchema = yup.InferType<typeof resetSchema>;
