d315b1e6d05c659c7ea11e97b7c0c6423af31005
Abdelrahman Mohamed <abdelrhmanaboyehia@gmail.com>
2026-06-08 23:28:43 +0300

    create AuthContext

---
+++ src/Contexts/AuthContext.tsx
@@ -0,0 +1,44 @@
+import { createContext, useEffect, useState, type ReactNode } from "react";
+import { jwtDecode } from "jwt-decode";
+export interface User {
+  userName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  phoneNumber: string;
+  country: string;
+  role: string;
+  profileImage?: string;
+  exp?: number;
+}
+interface AuthContextType {
+  userData: User | null;
+  saveUserData: () => void;
+}
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+interface AuthContextProvProp {
+  children: ReactNode;
+}
+export default function AuthContextProvider({ children }: AuthContextProvProp) {
+  const [userData, setUserData] = useState<User | null>(null);
+
+  const saveUserData = () => {
+    const encoded = localStorage.getItem("userToken");
+    if (encoded) {
+      const decoded = jwtDecode<User>(encoded);
+      setUserData(decoded);
+    }
+  };
+  useEffect(() => {
+    if (localStorage.getItem("userToken")) {
+      saveUserData();
+    }
+  }, []);
+  return (
+    <AuthContext.Provider value={{ userData, saveUserData }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
