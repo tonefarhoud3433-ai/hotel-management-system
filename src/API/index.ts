7ea1a94d6a8a2de9987b08ead4f40faf5391bf78
Abdelrahman Mohamed <abdelrhmanaboyehia@gmail.com>
2026-06-11 00:59:15 +0300

    Admin Ads Api

--- src/API/index.ts
+++ src/API/index.ts
@@ -2,4 +2,5 @@ export * as axiosClient from "./axsiosClient";
 export * as Auth from "./modules/Auth";
 export * as AdminData from "./modules/AdminData";
 export * as AdminRooms from "./modules/AdminRooms";
+export * as AdminAds from "./modules/AdminAds";
 
